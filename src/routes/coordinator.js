53803545be50ae3f6ac43d2a0536f20f06609eee
hsilvioviana <hsilvioviana@hotmail.com>
2021-08-06 16:45:07 -0300

    Rotas Feitas

---
+++ src/routes/coordinator.js
@@ -0,0 +1,40 @@
+export const goToLogin = (history) => {
+
+    history.push("/login")
+}
+
+export const goToSignup = (history) => {
+
+    history.push("/signup")
+}
+
+export const goToForgotPassword = (history) => {
+
+    history.push("/forgot/password")
+}
+
+export const goToResetPassword = (history) => {
+
+    history.push("/reset/password")
+}
+
+export const goToHome = (history) => {
+
+    history.push("/")
+}
+
+export const goToProfile = (history) => {
+
+    history.push("/profile")
+}
+
+export const goToLogout = (history) => {
+
+    localStorage.clear()
+    history.push("/login")
+}
+
+export const goBack = (history) => {
+
+    history.goBack()
+}
